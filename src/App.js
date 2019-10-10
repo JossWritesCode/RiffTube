4b1ffa2df289ee9e56e3221bce7beb326aea7156
Joscelyn <JoscelynRose@gmail.com>
2019-10-09 20:44:29 -0400

    WIP: adding ability to chagne video based on input from form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [videoUrl, setVideoUrl] = useState('');
+
+  const handleChange = event => {
+    setVideoUrl(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(videoUrl);
+  };
+
   return (
     <div className="App">
+      {console.log(videoUrl)}
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Your Youtube Video:
+          <input
+            type="text"
+            name="videoUrl"
+            value={videoUrl}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
       <iframe
         title="video"
         width="420"
