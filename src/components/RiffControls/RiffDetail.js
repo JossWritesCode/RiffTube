b830037104e32a6574e9e66688cda1e8be1a8b32
Joscelyn <JoscelynRose@gmail.com>
2019-11-16 19:44:04 -0500

    added prettier settings. made some small formatting changes

--- src/components/RiffControls/RiffDetail.js
+++ src/components/RiffControls/RiffDetail.js
@@ -4,6 +4,7 @@ import { editRiff } from '../../actions/index.js';
 
 /* this component is where a user can edit their riff */
 function RiffDetail(props) {
+  console.log(props, 'RiffDetail props');
   return (
     <div className="riff-detail">
       <div style={{ backgroundColor: props.selected ? 'lightpink' : 'white' }}>
