5297f32a149729c60f199edd890d419366b9b156
Joscelyn <JoscelynRose@gmail.com>
2019-11-27 21:55:22 -0500

    added scrollbar to control panel

---
+++ src/components/Login/Login 2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { GoogleLogin } from 'react-google-login-component';
+import { setGoogleUser } from '../../actions/index.js';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <GoogleLogin
+          socialId="941154439836-s6iglcrdckcj6od74kssqsom58j96hd8.apps.googleusercontent.com"
+          className="google-login"
+          scope="profile"
+          fetchBasicProfile={false}
+          responseHandler={this.props.setGoogleUser}
+          buttonText="Login With Google"
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  setGoogleUser
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Login);
---
+++ src/components/RiffControls/EditControls 2.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Record from './Record.js';
+import TestButton from '../Login/TestButton.js';
+import RiffList from './RiffList.js';
+import EditRiff from './EditRiff.js';
+import RiffButton from './RiffButton.js';
+import {
+  EDIT_MODE,
+  EDIT_NEW_MODE,
+  PLAY_MODE,
+  PAUSE_MODE
+} from '../../actions/index.js';
+
+/*This component houses all of the riff buttons and the rifflist*/
+function EditControls(props) {
+  return (
+    <div>
+      <div>
+        <RiffButton type="audio" />
+        <RiffButton type="text" />
+        ||
+        <TestButton />
+        {props.mode == EDIT_MODE || props.mode == EDIT_NEW_MODE ? (
+          <EditRiff />
+        ) : null}
+      </div>
+      <div>
+        <RiffList />
+      </div>
+    </div>
+  );
+}
+
+let mapStateToProps = state => ({
+  mode: state.mode
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(EditControls);
