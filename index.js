21cc74134321effadc839b81299fc3c384210573
paxfeline <paxfeline@gmail.com>
2019-11-29 23:28:10 -0800

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server/index.js');
 
-const PORT = /*process.env.PORT ||*/ 3300;
+const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
