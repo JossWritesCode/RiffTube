e1d34c227528f916f01fe1ff89fc09e4f8a74cb9
Joscelyn <JoscelynRose@gmail.com>
2019-12-28 15:42:43 -0500

    fixed up the styling so that the control panel takes up whole height

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -4,25 +4,13 @@ import ViewInterface from './components/ViewInterface/ViewInterface';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends React.Component {
-
   render() {
     return (
       <Router>
         <div className="App">
-          <div className="top-section">
-            <div className="title-and-url">
-              <h1>RiffTube</h1>
-            </div>
-          </div>
           <div className="main-section">
-            <Route
-              exact path="/"
-              component={EditInterface}
-            />
-            <Route
-              path="/view/:videoID"
-              component={ViewInterface}
-            />
+            <Route exact path="/" component={EditInterface} />
+            <Route path="/view/:videoID" component={ViewInterface} />
           </div>
         </div>
       </Router>
