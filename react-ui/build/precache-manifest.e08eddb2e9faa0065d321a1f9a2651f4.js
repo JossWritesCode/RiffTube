8329f24f87fa7604e13811b7ba0895c9f4a32821
paxfeline <paxfeline@gmail.com>
2020-01-02 01:28:39 -0800

    css tweak + build

--- react-ui/build/precache-manifest.e08eddb2e9faa0065d321a1f9a2651f4.js
+++ react-ui/build/precache-manifest.e08eddb2e9faa0065d321a1f9a2651f4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "20eaa4f08be710d205c0937334e68a87",
+    "revision": "7df324860816d910d9557fe9bf26f333",
     "url": "/index.html"
   },
   {
-    "revision": "bb3de2b4afd437562c09",
-    "url": "/static/css/main.bbce40b4.chunk.css"
+    "revision": "a91680575710b9928d33",
+    "url": "/static/css/main.d0c9cd06.chunk.css"
   },
   {
     "revision": "b15bde37e343187cd60a",
     "url": "/static/js/2.79a62b02.chunk.js"
   },
   {
-    "revision": "bb3de2b4afd437562c09",
+    "revision": "a91680575710b9928d33",
     "url": "/static/js/main.2918d86f.chunk.js"
   },
   {
