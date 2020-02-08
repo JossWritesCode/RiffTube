2514ff0dadbeb42b937beaf43e08f8e5cf856a92
Joscelyn <JoscelynRose@gmail.com>
2020-02-07 20:24:37 -0500

    added some images and separated css into scss files

---
+++ react-ui/src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <section class="top-part">
+        <nav>
+          <a>Home</a>
+          <a>Start Riffing</a>
+        </nav>
+      </section>
+    </div>
+  );
+}
+
+export default LandingPage;
