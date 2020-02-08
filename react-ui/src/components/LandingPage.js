7eadec7d9c0f7d3cd40e656a207781fc6767564d
Joscelyn <JoscelynRose@gmail.com>
2020-02-07 20:39:32 -0500

    working on styling landing page

--- react-ui/src/components/LandingPage.js
+++ react-ui/src/components/LandingPage.js
@@ -2,11 +2,20 @@ import React from 'react';
 function LandingPage() {
   return (
     <div className="landing-page">
-      <section class="top-part">
+      <section className="top-part">
         <nav>
           <a>Home</a>
           <a>Start Riffing</a>
         </nav>
+        <div className="heading">
+          <span className="heading-primary-main">RiffTube:</span>
+          <span className="heading-primary-sub">
+            Make Your Voice Heard On Any YouTube Video
+          </span>
+          <a className="myBtn" class="btn portfolio-btn">
+            Get Started &#8594;
+          </a>
+        </div>
       </section>
     </div>
   );
