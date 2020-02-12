83eba8e087694d9e456b0cfa35f2929898ed26b7
paxfeline <paxfeline@gmail.com>
2020-02-11 22:30:24 -0800

    fiddling

--- react-ui/src/components/YouTubeVideo/YouTubeVideo.js
+++ react-ui/src/components/YouTubeVideo/YouTubeVideo.js
@@ -152,15 +152,16 @@ class YouTubeVideo extends React.Component {
               if (!riff.payload) { console.log( "empty payload error" ); return; } // DEBUG - SHOULD BE REMOVED
               var audioURL = URL.createObjectURL(riff.payload);
               //debugger;
-              //console.log( "play riff!" );
               for ( let i = 0; i < window.audioPlayersCount; i++ )
               {
                 let audio = window.audioPlayers[i];
+                console.log( "try at ", i );
                 if ( audio.inUse ) continue;
                 audio.src = audioURL;
                 audio.play();
-                audio.inUse = true;
+                audio.inUse = i;
 
+                console.log( "play riff! at ", i );
                 this.curRiff[index] = audio;
                 break;
               }
