3fbe1f754a8ae77b4b7e0eff28351d9ee7b4f0c2
Joscelyn <JoscelynRose@gmail.com>
2020-02-19 20:28:53 -0500

    removed RiffTube-backend folder

--- react-ui/src/components/ViewInterface/AuthorSelector.js
+++ react-ui/src/components/ViewInterface/AuthorSelector.js
@@ -2,46 +2,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleViewUserIdMuted } from '../../actions';
 
-class AuthorSelector extends React.Component
-{
-    renderNames()
-    {
-        var names = [];
-        const containsReducer = id => (ac, cur) => cur.id === id ? ac || true : ac || false;
-        const includes = ( arr, id ) => arr.reduce( containsReducer( id ), false );
+class AuthorSelector extends React.Component {
+  renderNames() {
+    var names = [];
+    const containsReducer = id => (ac, cur) =>
+      cur.id === id ? ac || true : ac || false;
+    const includes = (arr, id) => arr.reduce(containsReducer(id), false);
 
-        this.props.riffs.forEach( el =>
-            {
-                //console.log( "name", el.name, includes( names, el.user_id ) );
-                if ( ! includes( names, el.user_id ) )
-                    names.push( { name: el.name, id: el.user_id } );
-            });
-        return names.map( el => (
-            <div key={el.id} onClick={ () => this.props.toggleViewUserIdMuted( el.id ) } style={ { backgroundColor: this.props.mutedIDs[el.id] ? 'gray' : 'blue' } }>
-                { el.name }
-            </div>
-        ));
-    }
-    
-    render()
-    {
-        return (
-            <div>
-                {
-                    this.renderNames()
-                }
-            </div>
-        );
-    }
+    this.props.riffs.forEach(el => {
+      //console.log( "name", el.name, includes( names, el.user_id ) );
+      if (!includes(names, el.user_id))
+        names.push({ name: el.name, id: el.user_id });
+    });
+    return names.map(el => (
+      <div
+        key={el.id}
+        onClick={() => this.props.toggleViewUserIdMuted(el.id)}
+        style={{
+          backgroundColor: this.props.mutedIDs[el.id] ? 'gray' : 'blue'
+        }}
+      >
+        {el.name}
+      </div>
+    ));
+  }
+
+  render() {
+    return <div>{this.renderNames()}</div>;
+  }
 }
 
 const mapStateToProps = state => ({
-    riffs: state.riffs.all,
-    mutedIDs: state.viewMutedUserIDs
-  });
-  
-  const mapDispatchToProps = {
-    toggleViewUserIdMuted
-  };
+  riffs: state.riffs.all,
+  mutedIDs: state.viewMutedUserIDs
+});
+
+const mapDispatchToProps = {
+  toggleViewUserIdMuted
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AuthorSelector);
