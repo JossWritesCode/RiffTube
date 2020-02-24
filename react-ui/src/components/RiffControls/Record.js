af05d978034139cc003fe2b8c4bf2997845ffb5c
Joscelyn <JoscelynRose@gmail.com>
2020-02-23 20:18:00 -0500

    fixed issue with login button
    Co-authored-by: paxfeline<paxfeline@gmail.com>

--- react-ui/src/components/RiffControls/Record.js
+++ react-ui/src/components/RiffControls/Record.js
@@ -47,7 +47,7 @@ class Record extends React.Component {
               this.state.mediaRecorder.start();
             }}
           >
-            record
+            Record
           </button>
         );
       } else {
