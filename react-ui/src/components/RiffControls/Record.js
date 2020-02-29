05d343fb2d983b6df7c065deb248c172f9ee7b07
paxfeline <paxfeline@gmail.com>
2020-02-29 00:26:15 -0800

    still messy, but working better

--- react-ui/src/components/RiffControls/Record.js
+++ react-ui/src/components/RiffControls/Record.js
@@ -16,42 +16,7 @@ class Record extends React.Component {
       navigator.mediaDevices
         .getUserMedia({ audio: true, video: false })
         .then(stream => {
-          //assign to gumStream for later use 
-          //gumStream = stream;
           this.setState( { "gumStream": stream } );
-          /* use the stream */
-          var AudioContext = window.AudioContext          // Default
-              || window.webkitAudioContext;  // Safari and old versions of Chrome
-          var audioContext = new AudioContext();
-          var input = audioContext.createMediaStreamSource(stream);
-          //stop the input from playing back through the speakers 
-          input.connect(audioContext.destination) //get the encoding 
-          var encodingType = 'mp3';
-          var recorder = new window.WebAudioRecorder(input, {
-              workerDir: "lib/",
-              encoding: encodingType,
-              onEncoderLoading: (recorder, encoding) => {
-                  // show "loading encoder..." display 
-                  console.log("Loading " + encoding + " encoder...");
-              },
-              onEncoderLoaded: (recorder, encoding) => {
-                  // hide "loading encoder..." display
-                  console.log(encoding + " encoder loaded");
-              }
-          });
-          this.setState( { recorder } );
-          recorder.onComplete = (recorder, blob) => {
-            console.log("Encoding complete");
-            //createDownloadLink(blob, recorder.encoding);
-            this.props.saveTempAudio(blob, this.duration);
-          };
-          recorder.setOptions({
-              timeLimit: 120,
-              encodeAfterRecord: true,
-              mp3: {
-                  bitRate: 160
-              }
-          });
         }).catch(function(err) { //enable the record button if getUSerMedia() fails 
           console.log( "oops, can't get stream", err );
         });
@@ -61,20 +26,53 @@ class Record extends React.Component {
   render() {
     var ret; // var for the value to be returned
 
-    if (navigator.mediaDevices && this.state.recorder) {
+    if (navigator.mediaDevices) {
       if (!this.state.recordingState) {
         ret = (
           <button
             id="recordBtn"
             onClick={() => {
               this.setState({ recordingState: true });
-              this.chunks = [];
               this.startTime = Date.now();
               //this.state.mediaRecorder.start();
 
+              var AudioContext = window.AudioContext          // Default
+                  || window.webkitAudioContext;  // Safari and old versions of Chrome
+              var audioContext = new AudioContext();
+              var input = audioContext.createMediaStreamSource(this.state.gumStream);
+
+              var recorder = new window.WebAudioRecorder(input, {
+                workerDir: "lib/",
+                encoding: 'mp3',
+                onEncoderLoading: (recorder, encoding) => {
+                    // show "loading encoder..." display 
+                    console.log("Loading " + encoding + " encoder...");
+                },
+                onEncoderLoaded: (recorder, encoding) => {
+                    // hide "loading encoder..." display
+                    console.log(encoding + " encoder loaded");
+                }
+              });
+
+              //this.setState( recorder );
+              this.recorder = recorder;
+
+              recorder.onComplete = (recorder, blob) => {
+                console.log("Encoding complete");
+                //createDownloadLink(blob, recorder.encoding);
+                this.props.saveTempAudio(blob, this.duration);
+              };
+              recorder.setOptions({
+                  timeLimit: 120,
+                  encodeAfterRecord: true,
+                  mp3: {
+                      bitRate: 160
+                  }
+              });
+
               //start the recording process 
               console.log("Recording started");
-              this.state.recorder.startRecording();
+              recorder.startRecording();
             }}
           >
             Record
@@ -90,16 +88,22 @@ class Record extends React.Component {
               //this.state.mediaRecorder.stop();
 
               this.state.gumStream.getAudioTracks()[0].stop();
-              this.state.recorder.finishRecording();
+              this.recorder.finishRecording();
+
+              // create new stream
+              navigator.mediaDevices
+                .getUserMedia({ audio: true, video: false })
+                .then(stream => {
+                  this.setState( { "gumStream": stream } );
+                }).catch(function(err) { //enable the record button if getUSerMedia() fails 
+                  console.log( "oops, can't get stream", err );
+                });
             }}
           >
             stop
           </button>
         );
       }
-    } else if (navigator.mediaDevices && !this.state.recorder) {
-      debugger;
-      ret = <span>mediaRecorder failed to initialize</span>;
     } else ret = <span>navigator.mediaDevices not supported. sorry.</span>;
 
     return (
