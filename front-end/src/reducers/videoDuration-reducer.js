bf22f5bb02a25a96e2410d9535d2367223948f97
paxfeline <paxfeline@gmail.com>
2020-06-02 00:30:56 -0700

    compile warning cleanup

--- front-end/src/reducers/videoDuration-reducer.js
+++ front-end/src/reducers/videoDuration-reducer.js
@@ -1,20 +1,9 @@
 import {
-  RECEIVE_RIFF_LIST,
-  SAVE_RIFF_SUCCESS,
   SET_VIDEO_DURATION
 } from '../actions/index.js';
 
 const videoDurationReducer = (state = null, action) => {
   switch (action.type) {
-    /*
-    case RECEIVE_RIFF_LIST:
-    case SAVE_RIFF_SUCCESS: // this needs testing
-      console.log( "video duration reducer", action);
-      if ( action.payload.duration )
-        return action.payload.duration;
-      // else
-      return state;
-      */
     case SET_VIDEO_DURATION:
       return action.payload;
     default:
