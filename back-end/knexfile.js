047c4b1aa36c858352ca5a39ab460f9836da2383
paxfeline <paxfeline@gmail.com>
2021-03-31 19:55:21 -0700

    knex attempt

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -12,7 +12,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: true,
+    connection: {
+      host: process.env.DATABASE_URL,
+      ssl: true,
+    },
     migrations: {
       directory: './data/migrations'
     },
