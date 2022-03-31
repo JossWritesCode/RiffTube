09dfadf4fe8c071ccef3f9a34167f9c012ccdbc7
Joscelyn Owen <joscelynowen@Joscelyns-iMac.local>
2022-03-31 14:12:43 -0400

    chore: cleaned up Login

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -3,21 +3,17 @@ import { connect } from 'react-redux';
 import { GoogleLogin } from 'react-google-login';
 import { setGoogleUser } from '../../actions/index.js';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <GoogleLogin
-        clientId="941154439836-s6iglcrdckcj6od74kssqsom58j96hd8.apps.googleusercontent.com"
-        className="google-login"
-        isSignedIn={true}
-        onSuccess={(gus) => {
-          this.props.setGoogleUser(gus, this.props.videoID);
-        }}
-        buttonText="Login With Google"
-      />
-    );
-  }
-}
+const Login = ({ setGoogleUser, videoID }) => (
+  <GoogleLogin
+    clientId="941154439836-s6iglcrdckcj6od74kssqsom58j96hd8.apps.googleusercontent.com"
+    className="google-login"
+    isSignedIn
+    onSuccess={(gus) => {
+      setGoogleUser(gus, videoID);
+    }}
+    buttonText="Login With Google"
+  />
+);
 
 const mapStateToProps = (state) => ({
   videoID: state.videoID,
