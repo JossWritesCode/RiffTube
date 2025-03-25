349ba1738a810e131159adf34a7ea5ff213545f0
JossWritesCode <joscelynrose@gmail.com>
2025-03-25 19:09:32 -0400

    Burned it down and started fresh: v3 begins here

--- back-end/data/db.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile');
-
-const env = process.env.NODE_ENV || 'development';
-const configOptions = knexConfig[env];
-
-module.exports = knex(configOptions);
--- back-end/index.js
+++
@@ -1,6 +0,0 @@
-const server = require('./server/index.js');
-
-const PORT = process.env.PORT || 3300;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
