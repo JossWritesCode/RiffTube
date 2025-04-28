36649c3bd4fd5728ca7a25158697680ee9521018
paxfeline <paxfeline@gmail.com>
2025-04-27 21:55:17 -0700

    chore(test): added tests, one failing

--- backend/test/models/project_riff_test.rb
+++ backend/test/models/project_riff_test.rb
@@ -7,6 +7,7 @@ class ProjectRiffTest < ActiveSupport::TestCase
   #   Assert that a ProjectRiff without a project_id is invalid.
   test 'a ProjectRiff without a project_id is invalid' do
     project_riff = build(:project_riff)
+    assert_nil project_riff.project_id
     assert_not project_riff.valid?
     assert_includes project_riff.errors[:project_id], "can't be blank"
   end
@@ -21,10 +22,20 @@ class ProjectRiffTest < ActiveSupport::TestCase
   #   Assert that a riff_id must be unique per project_id (scoped uniqueness).
   test 'a riff_id must be unique per project_id (scoped uniqueness)' do
     project = create(:project)
-    project_riff_1 = create(:project_riff, project: project)
-    project_riff_2 = build(:project_riff, project: project, riff_id: project_riff_1.riff_id)
-    assert_not project_riff_2.valid?
-    assert_includes project_riff_2.errors[:riff_id], 'already linked to this project'
+    riff = create(:riff)
+    project_riff1 = create(:project_riff, project: project, riff: riff)
+    project_riff2 = build(:project_riff, project: project, riff_id: project_riff1.riff_id)
+    assert_not project_riff2.valid?
+    assert_includes project_riff2.errors[:riff_id], 'already linked to this project'
+
+    riff2 = create(:riff)
+    project_riff2 = build(:project_riff, project: project, riff: riff2)
+    assert_not_equal project_riff1.riff_id, project_riff2.riff_id
+    assert project_riff2.valid?
+
+    project2 = create(:project)
+    project_riff3 = create(:project_riff, project: project2, riff: riff)
+    assert project_riff3.valid?
   end
 
   #   Assert that start_time and end_time, if present, must be greater than or equal to 0.
@@ -45,6 +56,10 @@ class ProjectRiffTest < ActiveSupport::TestCase
     project_riff.start_time = 1
     project_riff.end_time = 2
     assert project_riff.valid?
+
+    # only numbers are allowed
+    project_riff.start_time = 'foo'
+    assert_includes project_riff.errors[:start_time], 'must be greater than or equal to 0'
   end
 
   #   Assert belongs_to :project.
