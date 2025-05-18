6a5a5300960f38de7dd5d77a99a66eef1d1eb149
paxfeline <paxfeline@gmail.com>
2025-05-17 23:38:25 -0700

    fixes for rubocop

--- backend/test/controllers/api/v1/omniauth/sessions_controller_test.rb
+++ backend/test/controllers/api/v1/omniauth/sessions_controller_test.rb
@@ -2,91 +2,93 @@
 
 require 'test_helper'
 
-# Tests for OmniAuth
-class OmniAuth::SessionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    OmniAuth.config.test_mode = true
-  end
+module OmniAuth
+  # Tests for OmniAuth
+  class SessionsControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      OmniAuth.config.test_mode = true
+    end
 
-  # Assert that a new user logging in creates a new user and redirects properly
-  test 'a user can sign up with a Google account' do
-    # New user logging in
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-      {
-        provider: 'google',
-        uid: '1234567890',
-        info: {
-          email: 'test@example.com',
-          name: 'Test User'
+    # Assert that a new user logging in creates a new user and redirects properly
+    test 'a user can sign up with a Google account' do
+      # New user logging in
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+        {
+          provider: 'google',
+          uid: '1234567890',
+          info: {
+            email: 'test@example.com',
+            name: 'Test User'
+          }
         }
-      }
-    )
+      )
 
-    # Assert one more user
-    assert_difference('User.count', 1) do
+      # Assert one more user
+      assert_difference('User.count', 1) do
+        get '/api/v1/auth/google_oauth2/callback'
+      end
+
+      # Should redirect here
+      assert_redirected_to '/api/v1/me'
+
+      # The following could be a sanity check, but may just be unecessary
+
+      # This should return the current user
+      # get '/api/v1/me' # Or: response.header['Location']
+
+      # assert_response :ok
+
+      # json = JSON.parse(response.body)
+      # assert_equal session[:user_id], json['user']['id']
+    end
+
+    # Assert that an existing user can log in
+    test 'a user can sign in with a Google account' do
+      # New user logging in
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+        {
+          provider: 'google',
+          uid: '1234567891',
+          info: {
+            email: 'joel@example.com',
+            name: 'Joel Hodgeson'
+          }
+        }
+      )
+
       get '/api/v1/auth/google_oauth2/callback'
+
+      # Should redirect here
+      assert_redirected_to '/api/v1/me'
+
+      # Find user and assert session id
+      user = User.find_by(email: 'joel@example.com')
+      assert_equal session[:user_id], user.id
+
+      # The following could be a sanity check, but may just be unecessary
+
+      # This should return the current user
+      # get '/api/v1/me' # Or: response.header['Location']
+
+      # assert_response :ok
+
+      # json = JSON.parse(response.body)
+      # assert_equal session[:user_id], json['user']['id']
     end
 
-    # Should redirect here
-    assert_redirected_to '/api/v1/me'
+    # Assert that failure redirects
+    test 'auth failure redirects' do
+      # New user logging in
+      OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
 
-    # The following could be a sanity check, but may just be unecessary
+      OmniAuth.config.on_failure = proc do |env|
+        OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+      end
 
-    # This should return the current user
-    # get '/api/v1/me' # Or: response.header['Location']
+      get '/api/v1/auth/google_oauth2/callback'
 
-    # assert_response :ok
-
-    # json = JSON.parse(response.body)
-    # assert_equal session[:user_id], json['user']['id']
-  end
-
-  # Assert that an existing user can log in
-  test 'a user can sign in with a Google account' do
-    # New user logging in
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-      {
-        provider: 'google',
-        uid: '1234567891',
-        info: {
-          email: 'joel@example.com',
-          name: 'Joel Hodgeson'
-        }
-      }
-    )
-
-    get '/api/v1/auth/google_oauth2/callback'
-
-    # Should redirect here
-    assert_redirected_to '/api/v1/me'
-
-    # Find user and assert session id
-    user = User.find_by(email: 'joel@example.com')
-    assert_equal session[:user_id], user.id
-
-    # The following could be a sanity check, but may just be unecessary
-
-    # This should return the current user
-    # get '/api/v1/me' # Or: response.header['Location']
-
-    # assert_response :ok
-
-    # json = JSON.parse(response.body)
-    # assert_equal session[:user_id], json['user']['id']
-  end
-
-  # Assert that failure redirects
-  test 'auth failure redirects' do
-    # New user logging in
-    OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
-
-    OmniAuth.config.on_failure = proc do |env|
-      OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+      # Should redirect to failure
+      assert_redirected_to '/auth/failure?message=invalid_credentials&strategy=google_oauth2'
     end
-
-    get '/api/v1/auth/google_oauth2/callback'
-
-    # Should redirect to failure
-    assert_redirected_to '/auth/failure?message=invalid_credentials&strategy=google_oauth2'
   end
 end
