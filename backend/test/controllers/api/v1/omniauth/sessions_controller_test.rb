8edad867f637bc90b6a8d4f8cc37816b2872f412
paxfeline <paxfeline@gmail.com>
2025-05-20 20:31:30 -0700

    fixed one test, removed one test

--- backend/test/controllers/api/v1/omniauth/sessions_controller_test.rb
+++ backend/test/controllers/api/v1/omniauth/sessions_controller_test.rb
@@ -9,49 +9,27 @@ module OmniAuth
       OmniAuth.config.test_mode = true
     end
 
-    # Assert that a new user logging in creates a new user and redirects properly
-    test 'a user can sign up with a Google account' do
-      # New user logging in
-      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
-        {
-          provider: 'google',
-          uid: '1234567890',
-          info: {
-            email: 'test@example.com',
-            name: 'Test User'
-          }
-        }
-      )
-
-      # Assert one more user
-      assert_difference('User.count', 1) do
-        get '/api/v1/auth/google_oauth2/callback'
-      end
-
-      # Should redirect here
-      assert_redirected_to '/api/v1/me'
-
-      # The following could be a sanity check, but may just be unecessary
-
-      # This should return the current user
-      # get '/api/v1/me' # Or: response.header['Location']
-
-      # assert_response :ok
-
-      # json = JSON.parse(response.body)
-      # assert_equal session[:user_id], json['user']['id']
-    end
-
     # Assert that an existing user can log in
     test 'a user can sign in with a Google account' do
       # New user logging in
-      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(
+      user = create(
+        :user,
+        email: 'joel@example.com',
+        password: 'secret123',
+        username: 'hax',
+        uid: '1234567890',
+        provider: 'google',
+        name: 'Joel Hodgeson'
+      )
+      puts 'wut'
+      puts user.inspect
+      OmniAuth.config.mock_auth[:google_oauth2] = AuthHash.new(
         {
           provider: 'google',
-          uid: '1234567891',
+          uid: user.uid,
           info: {
-            email: 'joel@example.com',
-            name: 'Joel Hodgeson'
+            email: user.email,
+            name: user.name
           }
         }
       )
